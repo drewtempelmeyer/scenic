5e25059b77c84a45b45a9a54a49761528f7147fb
Drew Tempelmeyer <drewtemp@gmail.com>
2012-04-01 20:54:10 -0400

    Initial commit. Need to figure out CSS vertical positioning...

---
+++ scenic.js
@@ -0,0 +1,142 @@
+/**
+  Scenic v 0.1
+  Drew Tempelmeyer (drewtemp@gmail.com)
+  ============================================
+  Scenic is a beautiful and elegant image viewer for your
+  beautiful images. Code responsibly.
+*/
+(function($) {
+
+  $.fn.scenic = function( options ) {
+
+    var settings = $.extend({
+      'theme': 'default'
+    }, options);
+
+    // Set a listener for the escape key to close scenic
+    $(document).keyup(function(e) {
+      if (e.keyCode == 27)
+        $.fn.scenic.close();
+    });
+
+    // Listen for window resize
+    $(window).resize(function() { $.fn.scenic._resize(); });
+
+    // Listener to close Scenic when the current image is clicked (or parent element)
+    $('#scenic ul li.active').live('click', function() { $.fn.scenic.close(); });
+
+    // Listener to navigate to the previous image
+    $('#scenic ul li.previous').live('click', function() { $.fn.scenic.previous(); });
+
+    // Listener to navigate to the next image
+    $('#scenic ul li.next').live('click', function() { $.fn.scenic.next(); });
+
+    var index = 0;
+    return this.each(function() {
+      var $this = $(this);
+      $this.data('scenic-theme', settings.theme);
+      $this.data('scenic-index', index);
+      index++;
+
+      // Listen to all click events to trigger the Scenic viewer
+      $this.live('click', function(e) {
+        e.preventDefault();
+        $.fn.scenic.open($(this));
+      });
+
+    });
+  };
+
+  /**
+  Return true if Scenic is active. false otherwise
+  */
+  $.fn.scenic.isVisible = function() {
+    return $('#scenic').length > 0;
+  };
+
+  /**
+  Close Scenic and remove all corresponding elements
+  */
+  $.fn.scenic.close = function() {
+    if ($.fn.scenic.isVisible()) {
+      var $scenic = $('#scenic');
+      $scenic.fadeOut(200, function() { $scenic.remove(); });
+    }
+  };
+
+  /**
+  Open Scenic and create ALL OF THE THINGS
+  */
+  $.fn.scenic.open = function( element ) {
+    var rel        = element.attr('rel');
+    var theme      = element.data('scenic-theme');
+    var index      = element.data('scenic-index');
+    // Set up the parent div
+    var $scenicDiv = $('<div />').attr({
+      'class' : theme,
+      'id'    : 'scenic'
+    });
+    var $scenicUl = $('<ul />');
+
+    $('a[rel="' + rel + '"]').each(function() {
+      var $a = $(this);
+      // I love parenthesis!
+      $scenicUl.append($('<li />').append($('<div />').append('<span />').append($('<img />').attr({ 'src': $a.attr('href'), 'title': $a.attr('title') }))));
+    });
+
+    // Finally append Scenic to the end of the body
+    $('body').append($scenicDiv.append($scenicUl));
+
+    var $scenicLis = $('#scenic ul li');
+
+    $($scenicLis.get(index)).addClass('active');
+
+    // If there is a previous image, add the previous class
+    if (index > 0)
+      $($scenicLis.get(index - 1)).addClass('previous');
+
+    // If there is a next image, add the next class
+    if (index != $scenicLis.length - 1)
+      $($scenicLis.get(index + 1)).addClass('next');
+
+    $.fn.scenic._resize();
+  };
+
+  /**
+  Show the previous image
+  */
+  $.fn.scenic.previous = function() {
+    var $prev = $('#scenic ul li.previous').prev('li');
+    $('#scenic ul li.next').removeClass('next');
+    $('#scenic ul li.active').removeClass('active').addClass('next');
+    $('#scenic ul li.previous').removeClass('previous').addClass('active');
+
+    if ($prev.length)
+      $prev.addClass('previous');
+  };
+
+  /**
+  Move to the next image
+  */
+  $.fn.scenic.next = function() {
+    var $next = $('#scenic ul li.next').next('li');
+    $('#scenic ul li.previous').removeClass('previous');
+    $('#scenic ul li.active').removeClass('active').addClass('previous');
+    $('#scenic ul li.next').removeClass('next').addClass('active');
+
+    if ($next.length)
+      $next.addClass('next');
+  };
+
+  /**
+  PRIVATE METHOD
+  Resizes Scenic on open and window resize
+  */
+  $.fn.scenic._resize = function() {
+    if ($.fn.scenic.isVisible()) {
+      $('#scenic ul li span').css('line-height', $(window).height());
+      //$('#scenic ul li span').css('line-height', $(window).height());
+    }
+  };
+
+})(jQuery);
